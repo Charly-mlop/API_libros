058d136481e7db4b296828f8675e0b4705d13610
Charly-mlop <carlosdayanmlopez@gmail.com>
2024-05-20 16:10:19 -0600

    Creacion de proyecto

---
+++ src/main/java/com/aluracursos/Charlyday/libreriavirtual/LibreriaVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.Charlyday.libreriavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/Charlyday/libreriavirtual/LibreriaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.Charlyday.libreriavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
